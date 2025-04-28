23120db1e7534703949abc1fb3a4b7d1f1ace84d
morin <therinkusoi@gmail.com>
2025-04-28 15:17:14 +0200

    Adding new files and some changes

---
+++ interfaces.ts
@@ -0,0 +1,7 @@
+export interface ErrorResponse {
+    error: string;
+}
+
+export interface SuccessResponse {
+    message: string;
+}
---
+++ logger.ts
@@ -0,0 +1,14 @@
+import { createLogger, format, transports } from 'winston';
+
+const logger = createLogger({
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    transports: [
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' })
+    ]
+});
+
+export default logger;
